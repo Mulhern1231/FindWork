539089c8c4ce0b1bad9ee97a79ce82bfc2ba6be4
Павел Волков <pavel@MacBook-Pro-Pavel.local>
2022-02-16 23:50:00 +0300

    add a new func

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ url = 'https://freelance.habr.com/tasks?q=%5Bpython%E2%80%8B%5D?q=%5Bpython%E2%8
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
 
-
 task = soup.find_all('div', class_='task__title')
 responses = soup.find_all('div', class_='task__params')
-
+list_habr = []
 for i in range(len(task)):
-    print(task[i].text)
-    print(responses[i].text)
-    print("\n")
+    list_habr.append(task[i].text + "\n" + responses[i].text + "\n" + "https://freelance.habr.com" + soup.select(f'#tasks_list > li:nth-child({i+1}) > article > div > header > div.task__title > a')[0].get('href'))
     
 
+list_habr.reverse()
+for text in list_habr:
+    print(text)
+    print("__________")
